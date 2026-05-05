a2c9ccaf201c3150844794656cb43343e34c5f54
Elias Holm <eliholm@kth.se>
2026-05-05 22:44:33 +0200

    test psuh

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black text-7xl">
       <h1>Hello!</h1>
+      <h6>ee</h6>
     </div>
   );
 }
